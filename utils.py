48d4623383bc68fd977f890cd7021848488d7194
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2023-11-26 22:49:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def find_match(input):
 
 def query_refiner(conversation, query):
     response = openai.Completion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
         temperature=0.7,
         max_tokens=256,
